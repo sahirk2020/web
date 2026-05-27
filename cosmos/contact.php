91f24c0c35d591f1f931856e0d48b35be4dbcee6
poojas-web <pooja.s@digitaltokri.com>
2026-05-27 12:15:28 +0530

    Updated contact us page and footer.

---
+++ cosmos/contact.php
@@ -0,0 +1,258 @@
+<?php include 'includes/header.php'; ?>
+<!--==============================
+    Breadcumb
+============================== -->
+<div class="breadcumb-wrapper" data-bg-src="assets/img/bg/breadcumb-bg.jpg">
+  <div class="container">
+    <div class="breadcumb-content">
+      <h1 class="breadcumb-title">Contact Us</h1>
+      <ul class="breadcumb-menu">
+        <li><a href="home-laundry-services.html">Home</a></li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!--==============================
+Contact Area   
+==============================-->
+<div class="space">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="title-area pe-xl-4 mb-30">
+          <h2 class="sec-title">
+            Revitalize Your Wardrobe Revive Your Day.
+          </h2>
+          <p class="">
+            Our standard turnaround time typically insert estimated time
+            here However, we also offer express services for faster
+            turnaround times if needed.
+          </p>
+        </div>
+        <div class="row gy-4 justify-content-center">
+          <div class="col-md-6">
+            <div class="contact-info text-center">
+              <div class="contact-info_icon">
+                <i class="fa-solid fa-phone"></i>
+              </div>
+              <div class="media-body ">
+                <h6 class="contact-info_title">Mobile Number</h6>
+                <span class="contact-info_text">
+                  <a href="tel:+9156129128">+9156-129-128</a>
+                  <!-- <a href="tel:+9156129128">+9156-129-128</a> -->
+                </span>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6">
+            <div class="contact-info text-center">
+              <div class="contact-info_icon">
+                <!-- <i class="fa-light fa-envelope"></i> -->
+                <i class="fa-solid fa-envelope"></i>
+              </div>
+              <div class="media-body ">
+                <h6 class="contact-info_title">Email Address</h6>
+                <span class="contact-info_text">
+                  <!-- <a href="mailto:info.example@gmail.com">help24/7@gmail.com</a> -->
+                  <a href="mailto:info@laun.com">info@laun.com</a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h5 class="mt-35">Follow The Social Media:</h5>
+        <div class="th-social">
+          <a href=" https://www.facebook.com/profile.php?id=100077069439683#"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/laundrycosmos/"><i class="fab fa-instagram"></i></a>
+          <!-- <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.behance.net/"><i class="fa-brands fa-behance"></i></a> -->
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <form
+          action="mail.php"
+          method="POST"
+          class="contact-form input-smoke ajax-contact card-shadow">
+          <h2 class="sec-title1 mb-30">Get In Touch</h2>
+          <div class="row">
+            <div class="form-group col-md-6">
+              <input
+                type="text"
+                class="form-control"
+                name="name"
+                id="name"
+                placeholder="Your Name" />
+              <i class="fal fa-user"></i>
+            </div>
+            <div class="form-group col-md-6">
+              <input
+                type="email"
+                class="form-control"
+                name="email"
+                id="email"
+                placeholder="Email Address" />
+              <i class="fal fa-envelope"></i>
+            </div>
+            <div class="form-group col-md-6">
+              <input
+                type="tel"
+                class="form-control"
+                name="number"
+                id="number"
+                placeholder="Phone Number" />
+              <i class="fal fa-phone"></i>
+            </div>
+            <div class="form-group col-md-6">
+              <select
+                name="subject"
+                id="subject"
+                class="form-select nice-select">
+                <option value="" disabled selected hidden>
+                  Select Service
+                </option>
+                <option value="Dry Cleaning">Dry Cleaning</option>
+                <option value="Wash & Fold">Wash & Fold</option>
+                <option value="Ironing/Pressing">Ironing/Pressing</option>
+                <option value="Garments Transformed">
+                  Garments Transformed
+                </option>
+                <option value="Household Textile Care">
+                  Household Textile Care
+                </option>
+                <option value="Household Textile Care">
+                  Household Textile Care
+                </option>
+              </select>
+            </div>
+            <div class="form-group col-12">
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="3"
+                class="form-control"
+                placeholder="Your Message"></textarea>
+              <i class="fal fa-pencil"></i>
+            </div>
+            <div class="form-btn col-12">
+              <button class="th-btn btn-fw th-radius">Send Message</button>
+            </div>
+          </div>
+          <p class="form-messages mb-0 mt-3"></p>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--==============================
+Price Area  
+==============================-->
+<section class="space-city">
+  <div class="container">
+    <div class="title-area text-center">
+      <h2 class="sec-title">Cities We Deliver to</h2>
+    </div>
+    <div class="row gy-4 justify-content-center">
+
+      <div class="col-xl-4 col-md-6">
+        <div class="price-box card-shadow text-center">
+          <div class="price-box_icon">
+            <img src="assets/img/icon/main-branch.png" alt="Icon">
+          </div>
+          <h3 class="box-title">Jalgaon</h3>
+          <h6 class="price-box_text">Pickup & Delivery Available</h6>
+          <!-- <div class="price-box_content">
+            <h4 class="price-box_price"><span class="currency">$</span>369. 00</h4>
+            <div class="available-list">
+              <ul>
+                <li>4 T-Shirts</li>
+                <li>1 Pairs of Jeans</li>
+                <li>3 Button-Down Shirts</li>
+                <li>1 Pair of Shorts</li>
+                <li>7 Pairs of Underwear</li>
+                <li>6 Pairs of Socks</li>
+                <li>1 Towel</li>
+              </ul>
+            </div>
+            <a href="contact.html" class="th-btn btn-fw th-radius">Order Now</a>
+          </div> -->
+        </div>
+      </div>
+
+      <div class="col-xl-4 col-md-6">
+        <div class="price-box card-shadow text-center">
+          <div class="price-box_icon">
+            <img src="assets/img/icon/location.png" alt="Icon">
+          </div>
+          <h3 class="box-title">Nashik</h3>
+          <h6 class="price-box_text">Same Day Service Available</h6>
+          <!-- <div class="price-box_content">
+            <h4 class="price-box_price"><span class="currency">$</span>420.00</h4>
+            <div class="available-list">
+              <ul>
+                <li>4 T-Shirts</li>
+                <li>2 Pairs of Jeans</li>
+                <li>3 Button-Down Shirts</li>
+                <li>5 Pair of Shorts</li>
+                <li>7 Pairs of Underwear</li>
+                <li>6 Pairs of Socks</li>
+                <li>6 Towel</li>
+              </ul>
+            </div>
+            <a href="contact.html" class="th-btn btn-fw th-radius">Order Now</a>
+          </div> -->
+        </div>
+      </div>
+
+      <!-- <div class="col-xl-4 col-md-6">
+        <div class="price-box">
+          <div class="price-box_icon">
+            <img src="assets/img/icon/price_1_3.svg" alt="Icon">
+          </div>
+          <h3 class="box-title">Premium Package</h3>
+          <h6 class="price-box_text">80 Cloth Per Month</h6>
+          <div class="price-box_content">
+            <h4 class="price-box_price"><span class="currency">$</span>400.00</h4>
+            <div class="available-list">
+              <ul>
+                <li>4 T-Shirts</li>
+                <li>3 Pairs of Jeans</li>
+                <li>3 Button-Down Shirts</li>
+                <li>6 Pair of Shorts</li>
+                <li>7 Pairs of Underwear</li>
+                <li>6 Pairs of Socks</li>
+                <li>3 Towel</li>
+              </ul>
+            </div>
+            <a href="contact.html" class="th-btn btn-fw th-radius">Order Now</a>
+          </div>
+        </div>
+      </div> -->
+
+    </div>
+  </div>
+</section>
+
+<!--==============================
+Map Area  
+==============================-->
+<div class="">
+  <div class="contact-map style2">
+    <!-- <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7310056272386!2d89.2286059153658!3d24.00527418490799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe9b97badc6151%3A0x30b048c9fb2129bc!2sAngfuztheme!5e0!3m2!1sen!2sbd!4v1651028958211!5m2!1sen!2sbd"
+      allowfullscreen=""
+      loading="lazy"></iframe> -->
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.8681586905404!2d75.55353887476845!3d20.997921388838012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd90f99c35e51c3%3A0xdb207799be67c5ca!2sCosmos%20Laundry!5e0!3m2!1sen!2sin!4v1779454337914!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+</div>
+
+
+<?php include 'includes/footer.php'; ?>
---
+++ cosmos/includes/footer.php
@@ -0,0 +1,247 @@
+<!--==============================
+	Footer Area
+==============================-->
+<div class="">
+    <div class="container z-index-common">
+        <div class="cta-wrap" data-overlay="title" data-opacity="9" data-bg-src="assets/img/bg/cta_bg_2.jpg">
+            <div class="row align-items-center">
+                <div class="col-xl-7 col-lg-6 mb-5 mb-lg-0">
+                    <div class="title-area mb-0 text-center text-lg-start">
+                        <span class="sub-title style1">Get Free Contact For Services</span>
+                        <h2 class="sec-title text-white">You Get Premium Laundry Service From Us!</h2>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-lg-6">
+                    <form class="cta-form">
+                        <div class="form-group">
+                            <input class="form-control" type="email" placeholder="Email Address" required="">
+                            <button type="submit" class="th-btn style4">Get your services</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+            <div class="cta-shape"><img src="assets/img/shape/line.png" alt=""></div>
+        </div>
+    </div>
+</div>
+
+<footer class="footer-wrapper footer-layout2" data-bg-src="assets/img/bg/footer_bg_2.jpg">
+    <div class="widget-area">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-md-6 col-xl-3">
+                    <div class="widget footer-widget">
+                        <div class="th-widget-about">
+                            <div class="about-logo">
+                                <a href="home-laundry-services.html"><img src="assets/img/logo-white.svg" alt="Laun"></a>
+                            </div>
+                            <p class="about-text">Begin by scheduling your laundry man service. You can choose from our is convenient options Any visible stains</p>
+                            <div class="th-social">
+                                <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+                                <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="col-md-6 col-xl-auto">
+                    <div class="widget widget_nav_menu footer-widget">
+                        <h3 class="widget_title">Our Services</h3>
+                        <div class="menu-all-pages-container">
+                            <ul class="menu">
+                                <li><a href="service.html">Dry Cleaning</a></li>
+                                <li><a href="service.html">Dust Removal</a></li>
+                                <li><a href="service.html">Damage Repair</a></li>
+                                <li><a href="service.html">Sanitize clothes</a></li>
+                                <li><a href="service.html"> Laundry Service</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div> -->
+
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget widget_nav_menu footer-widget">
+                        <h3 class="widget_title">Quick Links</h3>
+                        <div class="menu-all-pages-container">
+                            <ul class="menu">
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="project.html">Services</a></li>
+                                <li><a href="faq.html">Help & FAQs</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                                <li><a href="contact.html">Terms and Conditions</a></li>
+                                <li><a href="contact.html">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget footer-widget">
+                        <h3 class="widget_title">Contact Us</h3>
+
+                        <!-- <div class="th-widget-contact">
+                            <div class="info-box">
+                                <div class="info-box_icon">
+                                    <i class="fas fa-location-dot"></i>
+                                </div>
+                                <p class="info-box_text">4140 Parker Rd. Allentown, New Mexico 31134</p>
+                            </div>
+                            <div class="info-box">
+                                <div class="info-box_icon">
+                                    <i class="fas fa-phone"></i>
+                                </div>
+                                <p class="info-box_text">
+                                    <a href="tel:+16326543564" class="info-box_link">+(163)-2654-3564</a>
+                                    <a href="tel:+16326545432" class="info-box_link">+(163)-2654-5432</a>
+                                </p>
+                            </div>
+                            <div class="info-box">
+                                <div class="info-box_icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <p class="info-box_text">
+                                    <a href="mailto:help24/7@Laun.com" class="info-box_link">help24/7@Laun.com</a>
+                                </p>
+                            </div>
+                        </div> -->
+
+                        <div class="footer-info-wrapper">
+                            <div class="footer-info">
+                                <div class="footer-info_icon">
+                                    <!-- <i class="fa-sharp fa-solid fa-phone"></i> -->
+                                    <i class="fas fa-location-dot"></i>
+                                </div>
+                                <p class="info-box_text">4140 Parker Rd. Allentown, New Mexico 31134</p>
+                            </div>
+                            <div class="footer-info">
+                                <div class="footer-info_icon">
+                                    <i class="fa-sharp fa-solid fa-phone"></i>
+                                </div>
+                                <a class="text-inherit" href="tel:+256214203215">+256 214 203 215</a>
+                            </div>
+                            <div class="footer-info">
+                                <div class="footer-info_icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <a class="text-inherit" href="mailto:info@laun.com">info@laun.com</a>
+                            </div>
+                            <div class="footer-info">
+                                <div class="footer-info_icon">
+                                    <i class="fa-solid fa-clock"></i>
+                                </div>
+                                <span> Monday to Saturday: 9AM - 8PM</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- <div class="col-md-6 col-xl-auto">
+                    <div class="widget footer-widget">
+                        <h3 class="widget_title">Recent Posts</h3>
+                        <div class="recent-post-wrap">
+                            <div class="recent-post">
+                                <div class="media-img">
+                                    <a href="blog-details.html"><img src="assets/img/blog/recent-post-1-1.jpg" alt="Blog Image"></a>
+                                </div>
+                                <div class="media-body">
+                                    <div class="recent-post-meta">
+                                        <a href="blog.html"><i class="far fa-calendar"></i>21 October , 2023</a>
+                                    </div>
+                                    <h4 class="post-title"><a class="text-inherit" href="blog-details.html">A Fresh Start for Your Wardrobe</a></h4>
+                                </div>
+                            </div>
+                            <div class="recent-post">
+                                <div class="media-img">
+                                    <a href="blog-details.html"><img src="assets/img/blog/recent-post-1-2.jpg" alt="Blog Image"></a>
+                                </div>
+                                <div class="media-body">
+                                    <div class="recent-post-meta">
+                                        <a href="blog.html"><i class="far fa-calendar"></i>22 October , 2023</a>
+                                    </div>
+                                    <h4 class="post-title"><a class="text-inherit" href="blog-details.html">Where Clean Meets Convenience</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>-->
+                <!-- <div class="col-md-6 col-xl-auto">
+                    <div class="widget footer-widget">
+                        <h3 class="widget_title">Opening Our</h3>
+                        <div class="th-widget-schedule">
+                            <p class="schedule-info"><span>Mon - Tuesday</span><span>8:00 AM - 6:00 PM</span></p>
+                            <p class="schedule-info"><span>Wed:</span><span>9:00 AM - 5:00 PM</span></p>
+                            <p class="schedule-info"><span>Fri - Sat:</span><span>9:00 AM - 6:00 PM</span></p>
+                            <p class="schedule-info"><span>Sunday</span><span>Closed</span></p>
+                        </div>
+                    </div>
+                </div> -->
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="copyright-wrap">
+        <div class="container">
+            <div class="row gy-2 align-items-center">
+                <div class="col-lg-12">
+                    <p class="copyright-text text-center">Copyright <i class="fal fa-copyright"></i> <?php echo date("Y"); ?> <a href="home-laundry-services.html">Cosmos Laundry</a>. All Rights Reserved.</p>
+                </div>
+                <!-- <div class="col-lg-6 text-center text-md-end">
+                    <div class="footer-links">
+                        <ul>
+                            <li><a href="contact.html">Privacy Policy</a></li>
+                            <li><a href="contact.html">Terms &amp; Condition</a></li>
+                        </ul>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!--********************************
+			Code End  Here 
+	******************************** -->
+
+<!-- Scroll To Top -->
+<div class="scroll-top">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;">
+        </path>
+    </svg>
+</div>
+
+<!--==============================
+    All Js File
+============================== -->
+<!-- Jquery -->
+<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+<!-- Swiper Js -->
+<script src="assets/js/swiper-bundle.min.js"></script>
+<!-- Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Magnific Popup -->
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!-- Counter Up -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- Range Slider -->
+<script src="assets/js/jquery-ui.min.js"></script>
+<!-- imagesloaded -->
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+
+<!-- isotope -->
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<!-- nice select -->
+<script src="assets/js/nice-select.min.js"></script>
+
+<!-- Main Js File -->
+<script src="assets/js/main.js"></script>
+</body>
+
+</html>
