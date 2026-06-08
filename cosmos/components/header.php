345a6bdf67543cd425590560aae639d07939664c
pramila jadhav <pramila.j@digitaltokri.com>
2026-06-08 16:03:41 +0530

    Updated base URL and services.php in header

--- cosmos/components/header.php
+++ cosmos/components/header.php
@@ -127,7 +127,7 @@ include_once __DIR__ . '/../css.php';
                         <nav class="main-menu d-none d-lg-inline-block">
                             <ul>
                                 <li class="menu-item-has-children">
-                                    <a href="<?php echo BASE_URL; ?>home-laundry-services.php">Home</a>
+                                    <a href="<?php echo BASE_URL; ?>index.php">Home</a>
                                     <ul class="sub-menu">
                                         <li><a href="<?php echo BASE_URL; ?>home-laundry-services.php">Laundry Services</a></li>
                                         <li><a href="<?php echo BASE_URL; ?>home-dry-cleaning.php">Home Dry Cleaning</a></li>
@@ -138,7 +138,7 @@ include_once __DIR__ . '/../css.php';
                                 <li class="menu-item-has-children">
                                     <a href="#">Service</a>
                                     <ul class="sub-menu">
-                                        <li><a href="<?php echo BASE_URL; ?>service.php">Service</a></li>
+                                        <li><a href="<?php echo BASE_URL; ?>services.php">Service</a></li>
                                         <li><a href="<?php echo BASE_URL; ?>service-details.php">Service Details</a></li>
                                     </ul>
                                 </li>
