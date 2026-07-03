b74ba31f857d0c717c69d2f59879ffeec37db537
poojas-web <pooja.s@digitaltokri.com>
2026-07-03 11:39:24 +0530

    Mobile Responsive Chnges Done

--- cosmos/services/premium-wash.php
+++ cosmos/services/premium-wash.php
@@ -116,9 +116,9 @@
     <div class="breadcumb-wrapper services-banner" data-bg-src="<?php echo BASE_URL; ?>assets/img/banners/Premium-Wash.webp">
         <div class="container">
             <div class="breadcumb-content">
-                <h1 class="breadcumb-title">Premium Wash</h1>
+                <h1 class="breadcumb-title sec-title1">Premium Wash</h1>
                 <ul class="breadcumb-menu">
-                    <li><a href="home-laundry-services.html">Home</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>index.php">Home</a></li>
                     <li>Premium Wash</li>
                 </ul>
             </div>
